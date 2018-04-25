3afe764980f8f89637f839aebf06553b3d701e01
donkeykong91 <darylnator@gmail.com>
2018-04-25 00:20:36 -0700

    Used template strings and tag function

---
+++ ex4-template-strings.js
@@ -0,0 +1,22 @@
+function upper(strings,...values) {
+	var interpoLiteral = "";
+
+	for (let index = 0; index < strings.length; index++) {
+		if (index > 0) {
+			if (typeof values[index-1] == "string") {
+				interpoLiteral += values[index-1].toUpper();
+			}
+		}
+		interpoLiteral += strings[index];
+	}
+	return interpoLiteral;
+}
+
+var name = "kyle",
+	twitter = "getify",
+	classname = "es6 workshop";
+
+console.log(
+	upper`Hello ${name} (@${twitter}), welcome to the ${classname}!` ===
+	"Hello KYLE (@GETIFY), welcome to the ES6 WORKSHOP!"
+);
