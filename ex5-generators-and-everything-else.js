2048b030afe73d88df3a9c317093b990d26e0b82
donkeykong91 <darylnator@gmail.com>
2018-04-26 01:42:57 -0700

    Replaced calling function in last forloop with numbers object calling property of symbol.iterator and passing an object

--- ex5-generators-and-everything-else.js
+++ ex5-generators-and-everything-else.js
@@ -1,34 +1,17 @@
 var numbers = {
-	*[Symbol.iterator]() {
-		for (var number = this.start; number <= this.end; number + this.step) {
+	*[Symbol.iterator](
+											{
+												start: 0,
+												end: 100,
+												step: 1
+											}
+										)
+  {
+		for (var number = start; number <= end; number = number + step) {
 			yield number;
 		}
-	},
-	start: 0,
-	end: 100,
-	step: 1
-};
-
-function newNumbers(
-										{
-											start = 0,
-											end = 100,
-											step = 1
-										} = {}
-									 )
-{
-	return numbers =
-	{
-		*[Symbol.iterator]() {
-			for (var number = this.start; number <= this.end; number + this.step) {
-				yield number;
-			}
-		},
-		start,
-		end,
-		step
 	}
-}
+};
 
 // should print 0..100 by 1s
 for (let num of numbers) {
@@ -36,14 +19,14 @@ for (let num of numbers) {
 }
 
 // should print 6..30 by 4s
-for (let num of newNumbers(
-														{
-														  start: 6,
-														  end: 30,
-															step:4
-														}
-													 )
-													)
+for (let num of numbers[Symbol.iterator](
+																				 {
+																					 start: 6,
+																					 end: 30,
+																					 step: 4
+																				 }
+																			  )
+	  )
 {
 	console.log(num);
 }
